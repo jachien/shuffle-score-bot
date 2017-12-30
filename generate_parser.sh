ceb03892c3d1e608ec498e3494a79fc88f3d2987
Jeff Chien <jeff85@gmail.com>
2017-12-30 15:21:19 -0600

    add debug_lookahead flag

--- generate_parser.sh
+++ generate_parser.sh
@@ -1,1 +1,1 @@
-java -cp ~/code/javacc/target/javacc.jar javacc -debug_parser -output_directory=src/main/java/org/jchien/shuffle/parser src/main/resources/javacc/shufflerun.jj
+java -cp ~/code/javacc/target/javacc.jar javacc -debug_parser -debug_lookahead -output_directory=src/main/java/org/jchien/shuffle/parser src/main/resources/javacc/shufflerun.jj
