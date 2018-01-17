06f63861715b23a94c8e52d58a008deccc508b0b
Jeff Chien <jeff85@gmail.com>
2018-01-16 20:01:41 -0600

    add equals and hashCode implementation for Stage, also move stageId normalization method here

--- src/main/java/org/jchien/shuffle/model/Stage.java
+++ src/main/java/org/jchien/shuffle/model/Stage.java
@@ -1,5 +1,7 @@
 package org.jchien.shuffle.model;
 
+import java.util.Objects;
+
 /**
  * @author jchien
  */
@@ -18,5 +20,35 @@ public class Stage {
 
     public String getStageId() {
         return stageId;
+    }
+
+    public static String normalizeStageId(String stage) {
+        if (stage == null) {
+            return null;
+        }
+        return stage.replaceAll("\\s+", "").toLowerCase().trim();
+    }
+
+    @Override
+    public String toString() {
+        return "Stage{" +
+                "stageType=" + stageType +
+                ", stageId='" + stageId + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Stage stage = (Stage) o;
+        return stageType == stage.stageType &&
+                Objects.equals(stageId, stage.stageId);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(stageType, stageId);
     }
 }
