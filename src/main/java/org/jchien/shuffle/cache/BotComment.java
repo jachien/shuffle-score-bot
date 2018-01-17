6de04de34a7c3be6237fbb29af8a585cb7f646ac
Jeff Chien <jeff85@gmail.com>
2018-01-16 18:05:17 -0600

    add model class for storing (bot comment id, bot comment body)

---
+++ src/main/java/org/jchien/shuffle/cache/BotComment.java
@@ -0,0 +1,23 @@
+package org.jchien.shuffle.cache;
+
+/**
+ * @author jchien
+ */
+public class BotComment {
+    private final String commentId;
+
+    private final String content;
+
+    public BotComment(String commentId, String content) {
+        this.commentId = commentId;
+        this.content = content;
+    }
+
+    public String getCommentId() {
+        return commentId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
