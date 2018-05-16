2358fe26b1cfc9fe9a2e7c3ccf88ab01a3ea7d03
Jeff Chien <jeff85@gmail.com>
2018-05-15 21:42:41 -0500

    unused imports

--- src/main/java/org/jchien/shuffle/bot/ConfigUtils.java
+++ src/main/java/org/jchien/shuffle/bot/ConfigUtils.java
@@ -1,7 +1,5 @@
 package org.jchien.shuffle.bot;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
